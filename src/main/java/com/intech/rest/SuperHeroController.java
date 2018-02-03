6191af9ed31f4f45f73ded44810974bec085fa53
Stéphane <ralys@gmx.fr>
2018-02-03 15:42:04 +0100

    Handle Cors

--- src/main/java/com/intech/rest/SuperHeroController.java
+++ src/main/java/com/intech/rest/SuperHeroController.java
@@ -1,24 +1,20 @@
 package com.intech.rest;
 
-import java.sql.Blob;
-import java.sql.SQLException;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.h2.util.IOUtils;
+import com.intech.SuperHeroService;
+import com.intech.model.Superhero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ResourceUtils;
 import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.intech.SuperHeroService;
-import com.intech.model.Superhero;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/superhero")
 public class SuperHeroController {
 
