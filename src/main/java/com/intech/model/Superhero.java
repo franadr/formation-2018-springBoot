448766ebb910a1141fcd83161c4342bd1c7ff602
Stéphane <ralys@gmx.fr>
2018-03-19 18:13:36 +0100

    map imagesrc

--- src/main/java/com/intech/model/Superhero.java
+++ src/main/java/com/intech/model/Superhero.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -20,67 +17,43 @@ public class Superhero {
 	@Id
     @GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
-	
-	private String heroName;
-	
-	private String webscraperOrder;
-	
-	private int intelligence;
-	
-	private int strength;
-	
-	private int speed;
-	
-	private int durability;
-	
-	private int power;
-	
-	private int combat;
-	
+
+	@Column(name = "fullname")
 	private String fullName;
-	
-	private String alterEgos;
-	
-	private String aliases;
-	
+
+	@Column(name = "placeofbirth")
 	private String placeOfBirth;
-	
+
+	@Column(name = "image_src")
+	private String imageSrc;
+
+	private String heroName;
+	private String webscraperOrder;
+	private int intelligence;
+	private int strength;
+	private int speed;
+	private int durability;
+	private int power;
+	private int combat;
+	private String alterEgos;
+	private String aliases;
 	private String firstAppearance;
-	
 	private String publisher;
-	
 	private String alignment;
-	
 	private String gender;
-	
 	private String race;
-	
 	private String height;
-	
 	private String weight;
-	
 	private String eyes;
-	
 	private String hairs;
-	
 	private String occupation;
-	
 	private String base;
-	
 	private String teams;
-	
 	private String relatives;
-	
 	private String background;
-	
 	private String history;
-	
 	private String powers;
-	
 	private String equipments;
-	
 	private String weapons;
 
-	private String imageSrc;
-	
 }
