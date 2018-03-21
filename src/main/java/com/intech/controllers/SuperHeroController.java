1b28c24f8ffb8f503efa2b86f5a419061d1024e5
Arnaud GILLET <arnaud.gillet@intech.lu>
2018-03-21 10:57:44 +0100

    Use RestController and pretty print

--- src/main/java/com/intech/controllers/SuperHeroController.java
+++ src/main/java/com/intech/controllers/SuperHeroController.java
@@ -1,18 +1,25 @@
 package com.intech.controllers;
 
-import com.intech.dto.SuperheroDto;
-import io.swagger.annotations.ApiOperation;
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.intech.dto.SuperheroDto;
 import com.intech.services.SuperHeroService;
 
-import javax.validation.Valid;
+import io.swagger.annotations.ApiOperation;
 
-@Controller
+@RestController
 @CrossOrigin
 @RequestMapping("/superheroes")
 public class SuperHeroController {
@@ -22,29 +29,22 @@ public class SuperHeroController {
 	@Autowired
 	private SuperHeroService superHeroService;
 
-	@ApiOperation(
-			value = "Retrieve the full heroes list"
-	)
+	@ApiOperation(value = "Retrieve the full heroes list")
 	@GetMapping
-	public @ResponseBody Iterable<SuperheroDto> getAllSuperHeroes() {
+	public Iterable<SuperheroDto> getAllSuperHeroes() {
 		logger.info("GET /superheroes");
 		return superHeroService.findAllSuperHeroes();
 	}
 
-	@ApiOperation(
-			value = "Retrieve information about a specific superhero"
-	)
+	@ApiOperation(value = "Retrieve information about a specific superhero")
 	@GetMapping(value = "/{id}")
-	public @ResponseBody
-	SuperheroDto getASuperHero(@PathVariable Long id) {
+	public @ResponseBody SuperheroDto getASuperHero(@PathVariable Long id) {
 		logger.info("GET /superheroes/" + id);
 		return superHeroService.findSuperHeroById(id);
 		// TODO : Java formation => handle no such element Exception
 	}
 
-	@ApiOperation(
-			value = "Uptade information of a specific superhero"
-	)
+	@ApiOperation(value = "Uptade information of a specific superhero")
 	@PutMapping(value = "/{id}")
 	public void updateASuperHero(@PathVariable Long id, @RequestBody @Valid SuperheroDto superHero) {
 		logger.info("PUT /superheroes/" + id);
