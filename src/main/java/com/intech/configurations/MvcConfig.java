39822c8a65fb5ea3ad937788741c86eca6104f5b
clement.brunel <clement.brunel@intech.lu>
2018-04-17 00:48:58 +0200

    Fix Cors for OPTIONS with security enabled

---
+++ src/main/java/com/intech/configurations/MvcConfig.java
@@ -0,0 +1,16 @@
+
+package com.intech.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+    }
+
+} 
